ef1beddeb0867e304d59c32e8f05db22f69d81df
zulqurnain haider <zulqurnainjj@gmail.com>
2025-11-04 10:00:00 +0700

    initial expo setup

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  preset: 'jest-expo',
+  transformIgnorePatterns: [
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|@ant-design/react-native)',
+  ],
+  moduleNameMapper: {
+    '^@/src/(.*)$': '<rootDir>/src/$1',
+  },
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/**/__tests__/**',
+  ],
+  testMatch: [
+    '**/__tests__/**/*.(ts|tsx|js)',
+    '**/*.(test|spec).(ts|tsx|js)',
+  ],
+};
